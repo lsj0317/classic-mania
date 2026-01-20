4ccc71b97400ac14e569bfdb2b4681866260e934
lsj0317 <lhd0317@hanmail.net>
2026-01-21 03:14:44 +0900

    fix: update base path and switch to HashRouter3

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // 레포지토리 이름이 classic-mania이므로 아래와 같이 설정합니다.
-  base: "/classic-mania/",
+  base: "/classic/",
 })
